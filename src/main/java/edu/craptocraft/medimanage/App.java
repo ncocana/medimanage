e9ddd8e4fdf3f4a5555857617a17ca9281362810
ncocana <ncocana@cifpfbmoll.eu>
2023-05-17 18:06:42 +0200

    feat(templates, static, App, WebController): create frontend

--- src/main/java/edu/craptocraft/medimanage/App.java
+++ src/main/java/edu/craptocraft/medimanage/App.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import edu.craptocraft.medimanage.entity.Doctors;
 import edu.craptocraft.medimanage.entity.Medicines;
@@ -32,26 +33,31 @@ public class App implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Doctors doctor1 = new Doctors("arodriguez@gmail.com", "password", "Alejandro Rodríguez", LocalDate.of(2023, 05, 15), 1);
-        Doctors doctor2 = new Doctors("jpiedra@gmail.com", "password", "José Piedra", LocalDate.of(2023, 05, 15), 2);
-        Doctors doctor3 = new Doctors("flawrence@gmail.com", "password", "Fernando Lawrence", LocalDate.of(2023, 05, 15), 3);
-        repoDoctors.save(doctor1);
-        repoDoctors.save(doctor2);
-        repoDoctors.save(doctor3);
+        try {
+            Doctors doctor1 = new Doctors("arodriguez@gmail.com", "password", "Alejandro Rodríguez", LocalDate.of(2023, 05, 15), 1);
+            Doctors doctor2 = new Doctors("jpiedra@gmail.com", "password", "José Piedra", LocalDate.of(2023, 05, 15), 2);
+            Doctors doctor3 = new Doctors("flawrence@gmail.com", "password", "Fernando Lawrence", LocalDate.of(2023, 05, 15), 3);
+            repoDoctors.save(doctor1);
+            repoDoctors.save(doctor2);
+            repoDoctors.save(doctor3);
 
-        Medicines medicine1 = new Medicines("Alinex", 10.0f, 5.0f);
-        Medicines medicine2 = new Medicines("Lenix", 15.0f, 2.0f);
-        Medicines medicine3 = new Medicines("Preditinol", 20.0f, 3.0f);
-        repoMedicines.save(medicine1);
-        repoMedicines.save(medicine2);
-        repoMedicines.save(medicine3);
+            Medicines medicine1 = new Medicines("Alinex", 10.0f, 5.0f);
+            Medicines medicine2 = new Medicines("Lenix", 15.0f, 2.0f);
+            Medicines medicine3 = new Medicines("Preditinol", 20.0f, 3.0f);
+            repoMedicines.save(medicine1);
+            repoMedicines.save(medicine2);
+            repoMedicines.save(medicine3);
 
-        Patients patient1 = new Patients("aklutznovich@gmail.com", "Ajax Klutznovich");
-        Patients patient2 = new Patients("dragnvindr@gmail.com", "Diluc Ragnvindr");
-        Patients patient3 = new Patients("kalberich@gmail.com", "Kaeya Alberich");
-        repoPatients.save(patient1);
-        repoPatients.save(patient2);
-        repoPatients.save(patient3);
+            Patients patient1 = new Patients("aklutznovich@gmail.com", "Ajax Klutznovich");
+            Patients patient2 = new Patients("dragnvindr@gmail.com", "Diluc Ragnvindr");
+            Patients patient3 = new Patients("kalberich@gmail.com", "Kaeya Alberich");
+            repoPatients.save(patient1);
+            repoPatients.save(patient2);
+            repoPatients.save(patient3);
+        } catch (DataIntegrityViolationException e) {
+            // Initial data has already been created.
+            // Do nothing.
+        }
     }
 
 }
--- src/main/java/edu/craptocraft/medimanage/controller/WebController.java
+++ src/main/java/edu/craptocraft/medimanage/controller/WebController.java
@@ -1,12 +1,30 @@
 package edu.craptocraft.medimanage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import edu.craptocraft.medimanage.service.DoctorsService;
+import edu.craptocraft.medimanage.service.MedicinesService;
+import edu.craptocraft.medimanage.service.PatientsService;
+import edu.craptocraft.medimanage.service.PrescriptionsService;
+
 @RestController
 public class WebController {
+
+    @Autowired
+    private PrescriptionsService servicePrescription;
+    
+    @Autowired
+    private DoctorsService serviceDoctor;
+    
+    @Autowired
+    private MedicinesService serviceMedicine;
+    
+    @Autowired
+    private PatientsService servicePatient;
 
     @RequestMapping("/")
     public RedirectView redirect() {
@@ -15,22 +33,20 @@ public class WebController {
 
     @RequestMapping("/home")
     public ModelAndView index() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("home");
+        ModelAndView modelAndView = new ModelAndView("home");
         return modelAndView;
     }
 
     @RequestMapping("/login")
     public ModelAndView login() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("login");
+        ModelAndView modelAndView = new ModelAndView("login");
         return modelAndView;
     }
 
     @RequestMapping("/gestion")
     public ModelAndView gestion() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("gestion");
+        ModelAndView modelAndView = new ModelAndView("gestion");
+        modelAndView.addObject("prescriptions", servicePrescription.getAll());
         return modelAndView;
     }
 
@@ -38,6 +54,9 @@ public class WebController {
     public ModelAndView alta() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("alta");
+        modelAndView.addObject("doctors", serviceDoctor.getAll());
+        modelAndView.addObject("medicines", serviceMedicine.getAll());
+        modelAndView.addObject("patients", servicePatient.getAll());
         return modelAndView;
     }
     
